656c1c6274df3399ec833c3b077bbb0754c65ba0
JT <jt@JTs-MacBook-Pro.home>
2024-03-11 22:20:36 -0400

    Vault

---
+++ Vault.js
@@ -0,0 +1,23 @@
+console.log("Hello World")
+
+// This is the actual COmbination Numbers which I might need to change
+const x = 2
+const y = 5
+const z = 1
+// These are the three variables 
+const Circle = 40-20
+const Square = 4*x
+const Triangle = 10*4
+// These are the combinationa
+const CombinationOne = Circle/x
+const CombinationTwo = Square*y
+const CombinationThree = Triangle-z
+
+console.log(CombinationOne)
+console.log(CombinationTwo)
+console.log(CombinationThree)
+// Pop-up Alerts
+alert("You have received this message because you have been chosen to open an important vault. Here is the secret combination:")
+alert("First # of the Combination: " + CombinationOne)
+alert("Second # of the Combination: " + CombinationTwo)
+alert("Third # of the Combination: " + CombinationThree)
